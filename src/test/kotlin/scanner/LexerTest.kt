b024c260f36d5cea658ec63f0f7a8a0a7be85b83
Maximilian Geiger <maximilian.geiger2@gmail.com>
2026-01-22 01:42:38 +0100

    add tests

--- src/test/kotlin/scanner/LexerTest.kt
+++ src/test/kotlin/scanner/LexerTest.kt
@@ -1,5 +1,26 @@
 package scanner
 
+import org.intitis.scanner.Lexer
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-class LexerTest
+class LexerTest {
+
+    @Test
+    fun simpleTest() {
+        val result = Lexer("2*x").tokenize()
+        assertEquals(result.size,3)
+    }
+
+    @Test
+    fun longerTest() {
+        val result = Lexer("212*(38+5)*x^23-3/2*x-44").tokenize()
+        assertEquals(result.size,19)
+    }
+
+    @Test
+    fun testFunction() {
+        val result = Lexer("2*sin(x)").tokenize()
+        assertEquals(result.size,6)
+    }
+}
